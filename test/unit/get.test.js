aae787357d7bfecd2ee601651f0e867632090eff
心叶 <yelloxing@gmail.com>
2019-08-26 18:01:44 +0800

    删除代码（重新设计）

--- test/unit/get.test.js
+++
@@ -1,9 +0,0 @@
-QUnit.test('get', 3, function () {
-
-    var object = { a: { b: [1, 2, 3] } };
-
-    equal(JSON.stringify(__.get(object, 'a.b')), "[1,2,3]");
-    equal(__.get(object, 'a["b"][1]'), 2);
-    equal(__.get(object, 'a.c', 'default'), 'default');
-
-});
