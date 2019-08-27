25a4267943c515c2467b1db5e3f0cf63c441e3be
心叶 <yelloxing@gmail.com>
2019-08-27 16:41:25 +0800

    get和set添加对比

--- test/benchmark/get.test.js
+++ test/benchmark/get.test.js
@@ -1,9 +1,29 @@
-JSLitmus.test('get', function () {
+JSLitmus.test('get <<<<<<<<<<<<<<<<<<<<<<<<<<<<', function () {
 
     __.get({
         a: {
             b: [1, 2, 3]
         }
     }, 'a["b"][1]');
+
+});
+
+JSLitmus.test('get[点的方式]', function () {
+
+    ({
+        a: {
+            b: [1, 2, 3]
+        }
+    }).a.b[1];
+
+});
+
+JSLitmus.test('get[中括号的方式]', function () {
+
+    ({
+        a: {
+            b: [1, 2, 3]
+        }
+    })['a']['b'][1];
 
 });
--- test/benchmark/set.test.js
+++ test/benchmark/set.test.js
@@ -1,9 +1,29 @@
-JSLitmus.test('set', function () {
+JSLitmus.test('set <<<<<<<<<<<<<<<<<<<<<<<<<<<<', function () {
 
     __.get({
         a: {
             b: [1, 2, 3]
         }
     }, 'a["b"][1]', 10);
+
+});
+
+JSLitmus.test('set[点的方式]', function () {
+
+    ({
+        a: {
+            b: [1, 2, 3]
+        }
+    }).a.b[1] = 10;
+
+});
+
+JSLitmus.test('set[中括号的方式]', function () {
+
+    ({
+        a: {
+            b: [1, 2, 3]
+        }
+    })['a']['b'][1] = 10;
 
 });
