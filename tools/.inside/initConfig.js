ad4a8f29b14462a9760691bce28d27b799b2c9aa
小拳头 <3294539252@qq.com>
2019-09-06 22:32:33 +0800

    添加一些内部使用的工具方法

---
+++ tools/.inside/initConfig.js
@@ -0,0 +1,17 @@
+/**
+ * 初始化配置文件
+ * 
+ * @private
+ * @param {Json} init 默认值
+ * @param {Json} data
+ * @return {Json}
+ */
+export default function (init, data) {
+    for (let key in data)
+        try {
+            init[key] = data[key];
+        } catch (e) {
+            throw new Error("Illegal property value！");
+        }
+    return init;
+};
